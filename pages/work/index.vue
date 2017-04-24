bf170de04a5b06e8f5756e0d4ce3043f7971fb04
strothj <jason@jasons.io>
2017-04-24 13:31:09 -0400

    feature: add portfolio screenshots and meta info

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <p>work index</p>
+  <article>
+    <section-heading>My Work</section-heading>
+    <container>
+
+    </container>
+  </article>
 </template>
+
+<script>
+import Container from '~components/Container.vue';
+import SectionHeading from '~components/SectionHeading.vue';
+
+export default {
+  components: {
+    Container,
+    SectionHeading,
+  },
+};
+</script>
