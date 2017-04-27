3d2d28d61048948d9761c99b2856af046bc5954e
strothj <jason@jasons.io>
2017-04-27 17:47:53 -0400

    feature: add work route paths

---
+++ pages/work/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{ $route.params.slug }}</p>
+  </div>
+</template>
+
+<script>
+import work from '~assets/work.json';
+
+export default {
+  validate({ params }) {
+    const slugList = work.map(w => w.slug);
+    // return slugList.includes(params.slug.toLowerCase());
+    return slugList.indexOf(params.slug.toLowerCase()) !== -1;
+  },
+};
+</script>
